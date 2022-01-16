05cf71e4e7f9a9aab4d6266b7356f03d0367de08
VishnuNadella <63490737+VishnuNadella@users.noreply.github.com>
2022-01-16 16:40:53 +0530

    Update das_v2.py

--- das_v2.py
+++ das_v2.py
@@ -1,7 +1,6 @@
 # Required Modules for DAS Implementation
 from pyzbar.pyzbar import decode
 import gspread
-import time
 import streamlit as st
 from PIL import Image
 
@@ -12,10 +11,9 @@ sh = gc.open_by_key("1nW4f8seohxA24AS7vWTsrvCawztF7HXe6dFHMt-T0v8")
 
 # Searching for target in sheets using Binary Search
 def search(low, mid, high, data_set, name, ID, c):
+    # Accessing elements in sheets with the help of indentation since wks.get_all_values() will return list of lists
     while low <= high:
-        # print("Low:", low, "\nMid:", mid, "\nHigh:", high)
         mid = (high + low) // 2
-        # print("We are checking for:", int(data_set[mid][1][2:]), int(ID[2:]))
         if int(data_set[mid][1][2:]) < int(ID[2:]):
             low = mid + 1
         
@@ -32,8 +30,7 @@ def search(low, mid, high, data_set, name, ID, c):
     else: # Failed to find that element
         return False, None, None
 
-# Checks fletched data
-
+# Checks the status of the accuired data (from QR -> decoder function)
 def status(sheet, data_1, data_2, data_3, c): # Searching using Linear Search
     wks = sh.get_worksheet(sheet)
     data_set = wks.get_all_values()
@@ -43,36 +40,40 @@ def status(sheet, data_1, data_2, data_3, c): # Searching using Linear Search
     uqn_ID = data_1
     name = data_2
     required, sta, row = search(low, mid, high, data_set, name, uqn_ID, c)
-    if required and sta == 1:
+    if required and sta == 1: # Successful scan of a valid and unused QR code
         wks.update_cell(row, c + 1, "TRUE")
-        # print(f"send: {data_2} with ID: {data_1}")
-        output = data_2 + ' attendance set to ' + str(data_set[row][c + 1])
+        if c == 7:
+            cell_coord = "H" + str(c + 1)
+        elif c == 6:
+            cell_coord = "G" + str(c + 1)
+        output = data_2 + ' attendance set to ' + wks.acell(cell_coord).value
         print(output)
         st.success(output)
         st.balloons()
-        time.sleep(2)
         
-    elif required and sta == 0:
+        
+    elif required and sta == 0: # When someone uses the same QR for the 2nd time
         output = data_2 + ' is already present!'
         print(output)
         st.error(output)
-        time.sleep(2)
+        
 
-    elif sta == 2 or sta == None:
-        output = 'QR Not Matching with Database' + str(data_1) + " " + str(data_2) + " " + str(data_3)
+    elif sta == 2 or sta == None: # When someone creates their own QR code
+        output = 'QR Not Matching with Database ' + str(data_1) + " " + str(data_2) + " " + str(data_3)
         print(output)
         print(data_1, data_2, data_3)
         st.error(output)
-        time.sleep(2)
-    else:
+    else: # Worst Case sceneario
         output = "Program is dead..."
         print(output)
         st.error(output)
-        time.sleep(2)
-
+        
+# Decodes the QR code and directs to appropriate sub sheet
 def decoder(image):
-    barcode = decode(image)
-    for obj in barcode:
+    qrCode = decode(image) #decoded QR code
+    stat = 0
+    for obj in qrCode:
+        stat = 1
         barcodeData = obj.data.decode("utf-8")
         data_1, data_2, data_3 = map(str, barcodeData.split('\n'))
         if int(data_3[:5]) == 20951:
@@ -84,10 +85,13 @@ def decoder(image):
         else:
             output = "Provided QR code is Invalid"
             print(output)
-            st.error(output)  ## replace cam with st
-            time.sleep(2)
+            st.error(output)
+            
     else:
-        st.error("No QR code has been detected; Take a new pic")
+        if stat == 0:   # This if ladder is for overcoming a bug that's caused by for else
+            st.error("No QR code has been detected or recognized; Take a new pic")
+        elif stat == 1:
+            stat = 0
 
 
 # Page Configuration details for Streamlit
@@ -109,18 +113,10 @@ message = st.empty()
 # Camera Input Setup
 startcam = st.camera_input('Scan QR Code')
 
-##### FROM HERE 
-
 if startcam:
     img = Image.open(startcam)
     decoder(img)
 
-
-
-
-
-
-##### DONT CROSS THIS
 
 # Hiding Streamlit Components which are not necessary
 hide_st_style = """
